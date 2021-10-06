348ecfd16c110572449fec060b1430e314f0766b
Martin Carson <87444494+Jumbaba@users.noreply.github.com>
2021-10-06 09:47:18 -0700

    init commit

---
+++ src/urls/urls.controller.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const urls = require(path.resolve("src/data/urls-data"));
+
+//Middleware
+
+function isValidURL(req, res, next) {
+  const { data } = req.body;
+  if (data) {
+    res.locals.data = data;
+    next();
+  }
+  next({
+    status: 404,
+    message: `URL is missing`,
+  });
+}
+
+//Not Middleware
+
+function list(res) {
+  res.json({ data: urls });
+}
+
+function create(res) {
+  const data = res.locals.data;
+  const newUrlId = urls.length + 1;
+  const newUrl = {
+    ...data,
+    newUrlId,
+  };
+  
+  urls.push(newUrl);
+  res.status(201).json({ data: newUrl });
+}
+
+
+
+
+
+module.export={
+    list,
+    create:[isValidURL, create],
+
+}
